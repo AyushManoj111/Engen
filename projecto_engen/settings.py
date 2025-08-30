92e46f12bbd4f0d2525df601c048677ce3a77d74
Ayush Manoj <ayushmk69@gmail.com>
2025-08-30 11:19:46 +0200

    Better UI/UX

--- projecto_engen/settings.py
+++ projecto_engen/settings.py
@@ -92,19 +92,19 @@ WSGI_APPLICATION = "projecto_engen.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 # DB LOCAL
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 
 # DB RENDER
-#DATABASES = {
-#    "default": dj_database_url.config(
-#        default=os.environ.get("DATABASE_URL")
-#    )
-#}
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 
 # Password validation
